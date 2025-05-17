71980845d217d5aba980b265e1c7ee403edd8cd5
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-17 10:10:10 +0700

    Create operator-penugasan.java

---
+++ operator-penugasan.java
@@ -0,0 +1,22 @@
+package com.mycompany.operatorpenugasan;
+
+/**
+ * Demonstrasi Operator Penugasan dalam Java
+ * @author hanif
+ * @contributor luqmanaru
+ */
+public class OperatorPenugasan {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        
+        // Pengisian nilai awal
+        a = 7;
+        b = 56;
+        
+        // Operator penugasan perkalian
+        b *= a;  // Sama dengan b = b * a
+        System.out.println("Hasil perkalian: " + b);
+    }
+}
